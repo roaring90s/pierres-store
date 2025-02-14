d919a0f864ce96a6316dd6a4a109067b53cc82a0
roring90s <contato.marcos90s@gmail.com>
2025-02-14 16:42:00 -0300

    layout_updates

--- js/components.js
+++ js/components.js
@@ -15,3 +15,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
     carregarComponente("header", "components/header.html");
 });
+
+
+let currentPage = 1;
+
+function togglePage() {
+    const page1 = document.querySelector('.page1');
+    const page2 = document.querySelector('.page2');
+    const arrow = document.querySelector('.arrow');
+
+    page1.classList.toggle('active');
+    page2.classList.toggle('active');
+
+    if (currentPage === 1) {
+        arrow.textContent = '◀'; 
+        arrow.classList.remove('right');
+        arrow.classList.add('left');
+        currentPage = 2;
+    } else {
+        arrow.textContent = '▶'; 
+        arrow.classList.remove('left');
+        arrow.classList.add('right');
+        currentPage = 1;
+    }
+}

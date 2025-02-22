563953e522b9a9fda537e70cd89424b434452db9
Marcos Sousa <141679803+roaring90s@users.noreply.github.com>
2025-02-22 14:27:52 -0300

    remote_server_db

--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'pierreshop',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,  
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0

bccb3810f11ffae5ef7d0a537c08be70f5d5b5f2
Marcos Sousa <141679803+roaring90s@users.noreply.github.com>
2025-05-30 19:03:45 -0300

    Update db.js

--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -7,9 +7,11 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
-  max: 10,
+  ssl: {
+    rejectUnauthorized: false // necessário no Render
+  }
 });
 
 module.exports = pool;
